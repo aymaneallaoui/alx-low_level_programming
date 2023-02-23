6748a3f6860954db6090cf0cd684144752ff083c
aymaneallaoui <aymanepraxe@gmail.com>
2023-02-23 08:20:01 -0800

    day 04

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,23 @@
 #include "main.h"
+
 /**
- * more_numbers - to print 10 times numbers from 0 to 14
- * Return: void
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
  */
+
 void more_numbers(void)
 {
-	int i = 0, j;
 
-	while (i < 10)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-		j = 0;
-
-		while (j <= 14)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
-			j++;
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-	}
-	_putchar('\n');
-	i++;
+			_putchar('\n');
+		}
 }
